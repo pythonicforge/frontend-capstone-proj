e21dfc65fb86af843c3957f461d7e6a6a51a9d9a
Hardik <168327453+pythonicforge@users.noreply.github.com>
2024-12-11 21:52:47 +0530

    added: user registartion form

--- src/pages/userOnboard/UserOnbord.jsx
+++ src/pages/userOnboard/UserOnbord.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React from 'react';
+import './userOnboard.css';
+import { Link } from "react-router-dom";
+
 
 const UserOnbord = () => {
   return (
-    <div>UserOnbord</div>
+   <>
+   <div className="goBackButtonContainer">
+    <Link to="/" className='goBackButton'>Go Back</Link>
+   </div>
+   <form action="" className="userForm">
+    <label htmlFor="figmaLinkContainer" className="figmaLinkLabel">Figma Link</label>
+    <input type="text" id="figmaLinkContainer" className="figmaLinkContainer" placeholder='Paste your link here'/>
+    <br />
+    <label htmlFor="studentNameContainer" className="studentNameLabel">Student Name</label>
+    <input type="text" id="studentNameContainer" className="studentNameContainer" placeholder='Name'/>
+    <br />
+    <label htmlFor="mentorNameContainer" className="mentorNameLabel">Mentors</label>
+    <select name="mentorNameContainer" id="mentorNameContainer" className="mentorNameContainer">
+      <option value="">Select an option</option>
+      <option value="Ajay Sharma">Rishabh Sharma</option>
+      <option value="Aryan Singhal">Rishabh Sharma</option>
+      <option value="Jai Gupta">Rishabh Sharma</option>
+      <option value="Rahul Kumar">Rishabh Sharma</option>
+      <option value="Rishabh Sharma">Rishabh Sharma</option>
+      <option value="Rishabh Sharma">Rishabh Sharma</option>
+      <option value="Rishabh Sharma">Rishabh Sharma</option>
+      <option value="Rishabh Sharma">Rishabh Sharma</option>
+      <option value="Rishabh Sharma">Rishabh Sharma</option>
+      <option value="Rishabh Sharma">Rishabh Sharma</option>
+      <option value="Rishabh Sharma">Rishabh Sharma</option>
+      <option value="Rishabh Sharma">Rishabh Sharma</option>
+      <option value="Rishabh Sharma">Rishabh Sharma</option>
+      <option value="Rishabh Sharma">Rishabh Sharma</option>
+    </select>
+    <br />
+    <label htmlFor="studenEmailContainer" className="studentEmailLabel">Student Email</label>
+    <input type="email" id="studentEmailContainer" className="studentEmailContainer" placeholder='Email'/>
+    <br />
+    <label htmlFor="studentQueriesContainer" className="studentQueriesLabel">Any Queries ?</label>
+    <textarea name="studentQueriesContainer" id="studentQueriesContainer" className="studentQueriesContainer" placeholder='Write your message here'></textarea>
+    <br />
+    <input type="button" value="Submit" className="submitButton" />
+   </form>
+   </>
   )
 }
 
