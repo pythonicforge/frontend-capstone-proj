8a4d7f99ec646122c96be283c261e05c4bc483c2
Rishabh Sharma <rishabhsharma@Rishabhs-MacBook-Pro.local>
2024-12-11 21:47:33 +0530

    homepage changed

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -5,9 +5,30 @@ const HomePage = () => {
   return (
     <>
         <div className="homeMainContainer">
-            <Nabvar />
+          <div className="circle"></div>
             <div className="homePageContentContainer">
-              
+              <div className="cardContainer">
+                <div className="topRow">
+                  <div className="firstCard">
+                    amsbd
+                  </div>
+                  <div className="secondCard"></div>
+                  <div className="thirdCardContainer">
+                    <div className="thirdCardContainerTopCard"></div>
+                    <div className="thirdCardContainerBottomCard"></div>
+                  </div>
+                </div>
+                <div className="bottomRow">
+                  <div className="thirdCardContainer">
+                    <div className="thirdCardContainerBottomCard"></div>
+                    <div className="thirdCardContainerTopCard"></div>
+                  </div>
+                  <div className="firstCard"></div>
+                  <div className="secondCard"></div>
+
+                </div>
+              </div>
+
             </div>
         </div>
     </>
